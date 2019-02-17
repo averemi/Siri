c00eaa7850f586f146f396711cd693eb615320f3
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2019-02-17 16:46:37 +0200

    ex00, ex01 done

---
+++ Siri/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Siri
+//
+//  Created by Anastasia Veremiichyk on 2/17/19.
+//  Copyright © 2019 Anastasia Veremiichyk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
